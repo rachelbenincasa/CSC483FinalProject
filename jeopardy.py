d2b3f5b6fe5d9038d17db1acb014033c57ef6872
Rachel Benincasa <97925972+rachelbenincasa@users.noreply.github.com>
2025-04-11 14:20:03 -0700

    started on preprocessing

--- jeopardy.py
+++ jeopardy.py
@@ -1,1 +1,35 @@
+import re
+from pathlib import Path
+import json
+from collections import defaultdict
+
+class Jeopardy:
+  def __init__(self):
+    # stop words to remove
+    self.stop_words = {'the', 'and', 'of', 'to', 'in', 'a', 'is', 'that', 'for'}
+    # articles will store (for each article)
+    # -original title 
+    # -cleaned title
+    # -original text
+    # -cleaned text
+    self.articles = []
+
+  def cleanup_text(self, text):
+    # lowercase
+    text = text.lower()
+    # remove punc
+    text = re.sub(r'[^\w\s]', '', text)
+    # split into words & remove any stop words
+    words = [word for word in text.split() if word not in self.stop_words]
+    return words
+
+  def add_article(self, original_title, original_text):
+    # adds the specific article to the articles dictionary
+    self.articles.append({
+      'original_title': original_title,
+      'cleaned_title': self.cleanup_text(original_title),
+      'original_text': original_text,
+      'cleaned_text': self.cleanup.text(original text)
+    })
+      
 
